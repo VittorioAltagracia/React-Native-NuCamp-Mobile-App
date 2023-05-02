f5fbe1b7d582396dd310eab29fa987bf10f0ddbb
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-05-01 20:56:20 -0500

    test

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -10,7 +10,7 @@ const ContactScreen = () => {
         <Text>1 Nucamp Way</Text>
         <Text>Seattle, WA 98001</Text>
         <Text style={{ marginBottom: 10 }}>U.S.A.</Text>
-        <Text>Phone: 1-206-555-1234</Text>
+        <Text>Phone: 1-206-555-12434</Text>
         <Text>Email: campsites@nucamp.co</Text>
       </Card>
     </ScrollView>
