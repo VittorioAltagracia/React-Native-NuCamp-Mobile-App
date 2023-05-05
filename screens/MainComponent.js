0ead9e950f45122b52e227b2a724383aec29083e
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-05-04 20:11:49 -0500

    updated all components to use json data

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -13,6 +13,12 @@ import AboutScreen from "./AboutScreen";
 import ContactScreen from "./ContactScreen";
 import { Icon } from "react-native-elements";
 import logo from "../assets/images/logo.png";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchPartners } from "../features/partners/partnersSlice";
+import { fetchCampsites } from "../features/campsites/campsitesSlice";
+import { fetchPromotions } from "../features/promotions/promotionsSlice";
+import { fetchComments } from "../features/comments/commentsSlice";
 
 const Drawer = createDrawerNavigator();
 // returns an object and screen components
@@ -135,6 +141,16 @@ const CustomDrawerContent = (props) => (
 );
 
 const Main = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+    dispatch(fetchPromotions());
+    dispatch(fetchPartners());
+    dispatch(fetchComments());
+  }, [dispatch]);
+
+  // passing dispatch function into dependancy array of useEffect hook - best practice!
   return (
     <View
       style={{
--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,1 +1,1 @@
-export const baseUrl = "http://172.20.8.27:3001/";
+export const baseUrl = "http://172.20.10.3:3001/";
