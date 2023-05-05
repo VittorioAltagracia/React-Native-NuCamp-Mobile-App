1474db7694e4a15296cdb2089f8e99b0be357bac
VittorioAltagracia <92496399+VittorioAltagracia@users.noreply.github.com>
2023-05-04 20:47:29 -0500

    added loading comp and error handling

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -2,6 +2,7 @@ import { ScrollView } from "react-native-gesture-handler";
 import { Avatar, Card, ListItem, Text } from "react-native-elements";
 import { useSelector } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
+import Loading from "../components/LoadingComponent";
 
 const Mission = () => {
   return (
@@ -24,11 +25,39 @@ const Mission = () => {
 const AboutScreen = () => {
   const partners = useSelector((state) => state.partners);
 
+  if (partners.isLoading) {
+    return (
+      <ScrollView>
+        <Mission />
+
+        {/* Looping through partners array to render a card for each */}
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Loading />
+        </Card>
+      </ScrollView>
+    );
+  }
+
+  if (partners.errMess) {
+    return (
+      <ScrollView>
+        <Mission />
+
+        {/* Looping through partners array to render a card for each */}
+        <Card>
+          <Card.Title>Community Partners</Card.Title>
+          <Card.Divider />
+          <Text> {partners.errMess} </Text>
+        </Card>
+      </ScrollView>
+    );
+  }
+
   return (
     <ScrollView>
       <Mission />
-
-      {/* Looping through partners array to render a card for each */}
       <Card>
         <Card.Title>Community Partners</Card.Title>
         <Card.Divider />
